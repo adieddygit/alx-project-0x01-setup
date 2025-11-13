0953d7deea93301c0eac8f397189449dd658534c
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-11-13 12:18:57 +0000

    Implemented the Users Card Components

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -17,18 +17,18 @@ const Users: React.FC<UsersPageProps> = ({users})=>{
                 ))}</div>
             </main>
         </div>
-    )    
+    );  
 };
 
 export async function getStaticProps(){
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const users = await response.json();
+    const users: UserProps[] = await response.json();
 
     return {
         props:{
-            users
-        }
-    }
-};
+            users,
+        },
+    };
+}
 
 export default Users;

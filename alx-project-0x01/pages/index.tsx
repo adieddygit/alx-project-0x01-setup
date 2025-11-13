af19228461ae619bcb5738a0f2925b1030d52cf0
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2025-11-13 03:58:39 +0000

    Set up navigation between pages

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,12 +1,13 @@
 import Header from "@/components/layout/Header";
+
 const Home: React.FC = ()=>{
   return(
     <div className="flex flex-col h-screen">
       <Header/>
-      <main className="grow flex items-center justify-center bg-linear-to-r from-500 to-purple-600">
+      <main className="flex-grow flex items-center justify-center bg-gradient-to-r from-500 to-purple-600">
         <div className="text-center">
           <h1 className="text-5xl font-bold text-white">Welcome to our Application</h1>
-          <p className="mt-4 text-xl text-white">We`re glad you`re here. Explore and enjoy your experience.</p>
+          <p className="mt-4 text-xl text-white">We're glad you're here. Explore and enjoy your experience.</p>
           <button className="mt-6 px-6 py-3 bg-white text-blue-500 rounded-full font-semibold hover:bg-graay-200 transition">Get Started</button>
         </div>
 
